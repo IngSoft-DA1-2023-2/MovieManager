88bab99c228393fa8467f30d763e72ba12b8b18c
Joaquin Mendez <joaquin.mendez@kinamic.com>
2023-09-01 12:12:48 -0300

    add movie and actors

---
+++ Domain/Actor.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain
+{
+    public class Actor
+    {
+        private string _name;
+
+        public string Name { 
+            get 
+            {
+                return _name;
+            }
+            set 
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("El nombre no puede ser vacio");
+                }
+                _name = value;
+            } 
+        }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Domain/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
